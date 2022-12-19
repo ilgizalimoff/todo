bf27fc4d6aadb4c3c2b7254f42d76bac6e987be1
ILGIZ <ilgizalimoff@yandex.ru>
2022-12-19 21:45:15 +0400

    post commit

---
+++ js/script.js
@@ -0,0 +1,42 @@
+let addMessage = document.querySelector('.message'),
+    addButton = document.querySelector('.add'),
+    todo = document.querySelector('.todo')
+let todoList = []
+if(localStorage.getItem('todo')){
+    todoList = JSON.parse(localStorage.getItem('todo'))
+    displayMessages()
+}
+addButton.addEventListener('click', function () {
+    let newTodo = {
+        todo: addMessage.value,
+        checked: false,
+        important: false,
+    }
+    todoList.push(newTodo)
+    displayMessages()
+    localStorage.setItem('todo', JSON.stringify(todoList))
+})
+function displayMessages() {
+    let displayMessage = ''
+    todoList.forEach(function (item, i) {
+        displayMessage += `
+        <li>
+            <input type='checkbox' id='item_${i}' ${item.checked? 'checked' : ''}>
+            <label for ='item_${i}'>${item.todo}</label>
+            <button id='item_${i}' class='delete'>Delete</button>
+        </li>
+        `
+        todo.innerHTML = displayMessage
+    })
+}
+todo.addEventListener('change', function(e){
+    let valueLabel = todo.querySelector('[for=' + 
+    e.target.getAttribute('id') + ']').innerHTML
+    todoList.forEach(function(item){
+        if(item.todo === valueLabel){
+            item.checked = !item.checked
+            localStorage.setItem('todo', JSON.stringify(todoList))
+        }
+        
+    })
+})
