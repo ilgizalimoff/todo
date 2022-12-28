6513e26f63b72bac78a08d28a72fac37522b566d
ILGIZ <ilgizalimoff@yandex.ru>
2022-12-28 17:09:44 +0400

    add delete func and media

--- js/script.js
+++ js/script.js
@@ -1,12 +1,9 @@
 let addMessage = document.querySelector('.message'),
     addButton = document.querySelector('.add'),
     todo = document.querySelector('.todo'),
-    todoList = []
+    todoList = localStorage.getItem('todo') ? JSON.parse(localStorage.getItem('todo')) : []
 
-if (localStorage.getItem('todo')) {
-    todoList = JSON.parse(localStorage.getItem('todo'))
-    displayMessages()
-}
+displayMessages()
 
 addButton.addEventListener('click', function () {
     let newTodo = {
@@ -14,21 +11,25 @@ addButton.addEventListener('click', function () {
         checked: false,
         important: false,
     }
+    addMessage.value = ''
     todoList.push(newTodo)
     displayMessages()
     localStorage.setItem('todo', JSON.stringify(todoList))
 })
 
 function displayMessages() {
+    if (todoList.length == 0) {
+        todo.innerHTML = ''
+    }
     let displayMessage = ''
     todoList.forEach(function (item, i) {
         displayMessage += `
-        <li >
-            <input type='checkbox' id='item_${i}' ${item.checked ? 'checked' : ''}>
-            <label class='${item.important ? 'important' : ''}'  for ='item_${i}'>${item.todo}</label>
-            <button id='item_${i}' class='delete'>Delete</button>
-        </li>
-        `
+            <li >
+                <input type='checkbox' id='item_${i}' ${item.checked ? 'checked' : ''}>
+                <label class='${item.important ? 'important' : ''}'  for ='item_${i}'>${item.todo}</label>
+                <button id='item_${i}' class='delete'>Delete</button>
+            </li>
+            `
         todo.innerHTML = displayMessage
     })
 }
@@ -42,6 +43,22 @@ todo.addEventListener('change', function (e) {
             localStorage.setItem('todo', JSON.stringify(todoList))
         }
     })
+})
+
+todo.addEventListener('click', function (e) {
+    let deleteValueBtn = e.target
+
+    if (deleteValueBtn.className == 'delete') {
+        let valueLabel = todo.querySelector('[for=' +
+            e.target.getAttribute('id') + ']').innerHTML
+
+        todoList = todoList.filter(function (item) {
+            return item.todo !== valueLabel
+        })
+        
+        displayMessages()
+        localStorage.setItem('todo', JSON.stringify(todoList))
+    }
 })
 
 todo.addEventListener('contextmenu', function (e) {
