1492392c1b9bae82ce3afdd60c0bfe5a0c6c686e
ILGIZ <ilgizalimoff@yandex.ru>
2022-12-28 15:21:57 +0400

    ready project

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 let addMessage = document.querySelector('.message'),
     addButton = document.querySelector('.add'),
-    todo = document.querySelector('.todo')
-let todoList = []
-if(localStorage.getItem('todo')){
+    todo = document.querySelector('.todo'),
+    todoList = []
+
+if (localStorage.getItem('todo')) {
     todoList = JSON.parse(localStorage.getItem('todo'))
     displayMessages()
 }
+
 addButton.addEventListener('click', function () {
     let newTodo = {
         todo: addMessage.value,
@@ -16,27 +18,39 @@ addButton.addEventListener('click', function () {
     displayMessages()
     localStorage.setItem('todo', JSON.stringify(todoList))
 })
+
 function displayMessages() {
     let displayMessage = ''
     todoList.forEach(function (item, i) {
         displayMessage += `
-        <li>
-            <input type='checkbox' id='item_${i}' ${item.checked? 'checked' : ''}>
-            <label for ='item_${i}'>${item.todo}</label>
+        <li >
+            <input type='checkbox' id='item_${i}' ${item.checked ? 'checked' : ''}>
+            <label class='${item.important ? 'important' : ''}'  for ='item_${i}'>${item.todo}</label>
             <button id='item_${i}' class='delete'>Delete</button>
         </li>
         `
         todo.innerHTML = displayMessage
     })
 }
-todo.addEventListener('change', function(e){
-    let valueLabel = todo.querySelector('[for=' + 
-    e.target.getAttribute('id') + ']').innerHTML
-    todoList.forEach(function(item){
-        if(item.todo === valueLabel){
+
+todo.addEventListener('change', function (e) {
+    let valueLabel = todo.querySelector('[for=' +
+        e.target.getAttribute('id') + ']').innerHTML
+    todoList.forEach(function (item) {
+        if (item.todo === valueLabel) {
             item.checked = !item.checked
             localStorage.setItem('todo', JSON.stringify(todoList))
         }
-        
+    })
+})
+
+todo.addEventListener('contextmenu', function (e) {
+    e.preventDefault()
+    todoList.forEach(function (item) {
+        if (item.todo === e.target.innerHTML) {
+            item.important = !item.important
+            displayMessages()
+            localStorage.setItem('todo', JSON.stringify(todoList))
+        }
     })
 })
